70c1e0128fe7a4a1e998d9839c09323f9ace56a4
Marc Petit <1001489.joan23@fje.edu>
2016-10-18 13:28:18 +0200

    Mis 10 inserts

---
+++ Consultas_Imagenes/inserts.sql
@@ -0,0 +1,32 @@
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici decathlon robada', '2016-10-17', '2016-10-17', 'L''Hospitalet de Llobregat', 'La han robado delante del colegio joan 23 de bellvitge', 'BTWIN', 'ROCKRIDER 300', 'Negra', NULL, 'La bici estaba atada a un arbol en el parque enfrente del colegio joan 23 de bellvige.', '123456', '1.png', 50.00);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici Orbea loki robada', '2016-10-17', '2016-10-10', 'Av. Carrilet Hospitalet', 'La bici estaba en frente de la sala salamandra', 'Orbea', 'Loki', 'Negra', 'Menos de 1 año', 'Me han robado una bici Orbea loki en la avinguda carrilet de hospitalet en frente de la sala salamandra , la deje alli el dia 10 de octubre.', '145236', '2.png', 25.00);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici Cannondale verde robada', '2016-10-17', '2016-10-01', 'Barcelona carrer nou de la rambla', 'Estaba por ultima vez en frente del apolo', 'Cannondale', 'Caadx Tiagra', 'Verde', 'Menos de 1 año', 'El dia 1 de octubre perdí alguien me robó la bici, estaba bien atada cerca del teatro apolo de paralelo de barcelona. 
+Se trata de una bici Cannondale Caadx Tiagra de color verde lima.
+Si alguien la encuentra que se ponga en contacto conmigo. Gracias.', '', '3.png', NULL);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Por favor ayuda ! me han robado mi bici', '2016-10-17', '2016-10-16', 'Barrio Sales Viladecans', 'Fui a comprar el pan y me la robaron en el barrio salas de viladecans', 'Orbita', 'Alfa', 'Blanca', 'Un par de años', 'La bici robada se trata de Orbita ALFA blanca para mujer de 18 velocidades. Si alguie la encuentra lo agradeceré. Gracias', 'B0189AP1HE', '4.png', 35.00);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('bici amarilla robada', '2016-10-17', '2016-10-13', 'Sagrada Familia Barcelona', 'Unos extrangeros me obligaron a darles la bicicleta', 'Wizard Industry', 'Helliot Soho', 'Amarilla', '1 año', 'Por favor si alguien encuentra una bici Wizard Industry Helliot Soho amarilla q se ponga en contacto conmigo.', NULL, '5.png', 5.00);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici robada en castelldefels', '2016-10-17', '2016-10-09', 'Paseo maritimmo castelldefels', 'Fui a comprar a una tienda y la bici desapareció', 'Trek', 'Marlin', 'Naranja', '1 año', 'Perdi la bici en el paseo maritimo de castelldefels, es una bici Trek naranja.', NULL, '6.png', 15.00);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('bici decathlon verde robada en sant boi', '2016-10-17', '2016-10-16', 'Sant Boi de llobregat', 'Bici robada robada cerca de la estacion de tren de sant boi', 'BTWIN', ' ROCKRIDER 340', 'Verde', 'un par de años', 'Me robaron la bici cerca de la estacion de ferrocarril de sant boi.', NULL, '7.png', NULL);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici robada de un primer piso', '2016-10-17', '2016-10-17', 'Sabadell', 'Bici robada en el balcon de un primer piso', 'Giant', 'Talon', 'Azul', 'nueva', 'La bici la robaron del balcon de mi casa en un primer piso, en el centro de sabadell', '', '8.png', 10.50);
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici de montaña robada ', '2016-10-17', '2016-10-15', 'El Prat de llobregat', 'Bici robada en Sant Cosme', 'MMR', 'Woki', 'Roja', 'un año', 'La robaron de mi garaje', '', '9.png', );
+
+INSERT INTO bd_proyecto1.anunci (anu_titol, anu_data_anunci, anu_data_robatori, anu_ubicacio_robatori, anu_descripcio_robatori, anu_marca, anu_model, anu_color, anu_antiguitat, anu_descripcio, anu_numero_serie, anu_foto, anu_compensacio) 
+	VALUES ('Bici plegable Giant Expessway 2 Negra ', '2016-10-18', '2016-10-16', 'Playa del bogatell, barcelona', 'La bici estabada atada con tres candados en el párquing de bicis de la playa Bogatell. Enfrente del McDonalds.', 'Giant ', ' Expessway 2', 'Negra', 'un año', 'La bici estabada atada con tres candados en el párquing de bicis de la playa Bogatell. Enfrente del McDonalds.', '', '10.png', 15 );
+	
